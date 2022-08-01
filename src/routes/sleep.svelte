9fde6a016369a1088e195609659a6fca77dbf731
Andreas Nüßlein <andreas@nuessle.in>
2022-08-01 10:13:49 +0100

    add sleep route

---
+++ src/routes/sleep.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async ({ params, session }) => {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    return {};
+  };
+</script>
+
+<script lang="ts">
+    export let value;
+</script>
+
+<div>{value}</div>
+<input bind:value placeholder="Enter text"/>
